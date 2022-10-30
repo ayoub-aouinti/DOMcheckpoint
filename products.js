db1d65a50e571cfcbb148b90c31fea54d9737d8e
Ayoub <ayoubaouinti1990@gmail.com>
2022-10-30 10:02:10 +0100

    first commit

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirt 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Tshirt for men New Season 2022 adult men's casual sportswear.",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "T-shirt 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Tshirt for men New Season 2022 adult men's casual sportswear.",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Tshirt for men New Season 2022 adult men's casual sportswear.",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "T-shirt 4",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Tshirt for men New Season 2022 adult men's casual sportswear.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "T-shirt 5",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Tshirt for men New Season 2022 adult men's casual sportswear.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "T-shirt 6",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Tshirt for men New Season 2022 adult men's casual sportswear.",
+    imgSrc: "./img/t6.png",
+  },
+];
